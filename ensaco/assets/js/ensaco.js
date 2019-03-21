6a7fcadf0ea3a79e91031e74bfcf56c2bfbae6f4
Oggy <ognjen.djokic@gmail.com>
2019-03-21 21:51:48 +0100

    template page 1

--- ensaco/assets/js/ensaco.js
+++ ensaco/assets/js/ensaco.js
@@ -19,20 +19,20 @@ $(document).ready(function() {
             },
         });
 
-    //Loader, Scroll info & timeout for sim purposes
-    var scrollInfo = $('.scroll-info');
-    $(".loader").fadeOut(900, function() {
-        $(".container").fadeIn(700);
-        if ($(window).width() > 768) {
-            scrollInfo.fadeIn(300);
-            setTimeout(function() {
-                scrollInfo.fadeOut(300);
-            }, 1000);
-            setTimeout(function() {
-                $('.slide-content__title').slideDown(600);
-            }, 800);
-        }
-    });
+        //Loader, Scroll info & timeout for sim purposes
+        var scrollInfo = $('.scroll-info');
+        $(".loader").fadeOut(900, function() {
+            $(".container").fadeIn(700);
+            if ($(window).width() > 768) {
+                scrollInfo.fadeIn(300);
+                setTimeout(function() {
+                    scrollInfo.fadeOut(300);
+                }, 1000);
+                setTimeout(function() {
+                    $('.slide-content__title').slideDown(600);
+                }, 800);
+            }
+        });
     }, 300);
 
     //Menu trigger
@@ -40,7 +40,7 @@ $(document).ready(function() {
         $('nav').toggle();
     });
     //Menu toggle
-    $('.level-1 li').on('click', function(ev){
+    $('.level-1 li').on('click', function(ev) {
         $evTarget = $(ev.target);
         $evTarget.next('.level-2').slideToggle(200);
         $evTarget.find($('.ti-angle-down')).toggleClass('rotate');
@@ -49,13 +49,20 @@ $(document).ready(function() {
     });
 
     //Logo effect
-    $('.nav-bar__logo-img').mouseenter(function(){
-        $(this).attr('src',function(index, attr){
-            return attr.replace(".svg", "-hover.svg");});
+    $('.nav-bar__logo-img').mouseenter(function() {
+        $(this).attr('src', function(index, attr) {
+            return attr.replace(".svg", "-hover.svg");
+        });
     });
-    $('.nav-bar__logo-img').mouseleave(function(){
-        $(this).attr('src',function(index, attr){
-            return attr.replace("-hover.svg",".svg");});      
-        });    
-    
-}); 
+    $('.nav-bar__logo-img').mouseleave(function() {
+        $(this).attr('src', function(index, attr) {
+            return attr.replace("-hover.svg", ".svg");
+        });
+    });
+
+    //page block height
+    if ($(window).width > 768) {
+        var docH = $(document).height();
+        $('.ens-article__right-block').height(docH);
+    };
+});
